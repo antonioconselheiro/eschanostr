e1b3d50df274a178b83e2b7fb08aa65626cc9d97
António Conselheiro <anarcanudos@proton.me>
2024-11-01 14:05:09 -0300

    drafting eschalostr in rust

---
+++ README.md
@@ -0,0 +1,25 @@
+> "He that hath an ear, let him hear what the Spirit saith unto the
+> churches; To him that overcometh will I give to eat of the hidden
+> manna, and will give him a white stone, and in the stone a new name
+> written, which no man knoweth saving he that receiveth it."
+> Apocalypse 2:17
+
+# Eschalostr
+
+Convert electricity into read friendly nostr npub.
+
+![Animated GIF of Dr Neo Cortex from Crash Bandicoot series dancing russian dance kazotsky](./dr-neo-cortex-kazotsky.gif)
+
+## Release
+
+Not released yet
+
+## Donate
+
+Lighting donate: <a href="lightning:antonioconselheiro@getalby.com">lightning:antonioconselheiro@getalby.com</a>
+
+![zap me](https://raw.githubusercontent.com/antonioconselheiro/antonioconselheiro/main/img/qrcode-wallet-lighting.png)
+
+Bitcoin onchain donate: <a href="bitcoin:bc1qrm99lmmpwk7zsh7njpgthw87yvdm38j2lzpq7q">bc1qrm99lmmpwk7zsh7njpgthw87yvdm38j2lzpq7q</a>
+
+![zap me](https://raw.githubusercontent.com/antonioconselheiro/antonioconselheiro/main/img/qrcode-wallet-bitcoin.png)
---
+++ src/main.rs
@@ -0,0 +1,49 @@
+use nostr::prelude::*;
+use clap::Parser;
+use regex::Regex;
+use chrono::Local;
+
+#[derive(Parser)]
+struct Cli {
+
+    #[arg(short, long)]
+    regex: String,
+
+    #[arg(short, long)]
+    password: String,
+}
+
+fn main() {
+    let args = Cli::parse();
+
+    let full_regex_pattern = format!("^npub1({})$", args.regex);
+    let re = Regex::new(&full_regex_pattern)
+        .map_err(|e| {
+            eprintln!("Erro ao compilar a expressão regular: {}", e);
+            e
+        })?;
+
+    let mut my_keys: Keys;
+    let mut bech32_pubkey: String;
+    let ncryptsec: String;
+
+    loop {
+        // keys
+        let secret_key = Keys::generate();
+        
+        // npub
+        bech32_pubkey = my_keys.public_key().to_bech32()?;
+
+        // check if matches
+        if re.is_match(&bech32_pubkey) {
+
+            // ncryptsec
+            let ncryptsec = EncryptedSecretKey::new(&secret_key.secret_key(), args.password, 16, KeySecurity::Medium).unwrap();
+
+            println!("--------------------");
+            println!("Timestamp: {}", Local::now().to_rfc3339());
+            println!("Npub: {}", bech32_pubkey);
+            println!("Ncryptsec: {}", ncryptsec.to_bech32())?;
+        }
+    }
+}
