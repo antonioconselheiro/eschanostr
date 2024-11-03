66b7f82b1889b2f90070dfbb9e9af49354c3dab6
António Conselheiro <91137293+antonioconselheiro@users.noreply.github.com>
2024-11-03 12:15:03 -0300

    Update download-install.sh
    latest tag does not work

--- download-install.sh
+++ download-install.sh
@@ -1,4 +1,4 @@
 #bin/bash
-wget https://github.com/antonioconselheiro/eschanostr/releases/download/latest/eschanostr
+wget https://github.com/antonioconselheiro/eschanostr/releases/download/1.0.0/eschanostr
 mv eschanostr /usr/bin/eschanostr
 chmod +x /usr/bin/eschanostr
