ab6bc4f95c61bd3dbe1376cd66fafd1bc56c854c
António Conselheiro <anarcanudos@proton.me>
2024-11-03 07:34:25 -0300

    fix n guy animation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use crossterm::cursor::{RestorePosition, SavePosition};
+use crossterm::queue;
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 use nostr::prelude::*;
 use clap::Parser;
 use regex::Regex;
@@ -9,12 +12,10 @@ use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
 use crossterm::{
-    cursor::{MoveTo, Hide, Show},
-    terminal::{Clear, ClearType},
-    execute,
-    style::Print,
+  cursor::MoveTo,
+  style::Print,
 };
-use std::io::stdout;
+use std::io::{stdout, Write};
 
 #[derive(Parser)]
 struct Cli {
@@ -51,108 +52,129 @@ const FRAMES: [&str; 13] = [
   FRAME5
 ];
 
-fn animate_character(running: Arc<AtomicBool>, row: u16, col: u16) {
-    let mut stdout = stdout();
+struct AnimationThread {
+  running: Arc<AtomicBool>,
+  handle: Option<thread::JoinHandle<()>>,
+}
+
+impl AnimationThread {
+  fn new() -> Self {
+    AnimationThread {
+          running: Arc::new(AtomicBool::new(false)),
+          handle: None,
+      }
+  }
+
+  fn start(&mut self, row: u16, col: u16) {
+    self.running.store(true, Ordering::Relaxed);
+    let running = self.running.clone();
     
-    // Hide cursor during animation
-    execute!(stdout, Hide).unwrap();
+    self.handle = Some(thread::spawn(move || {
+      let mut stdout = stdout();
+      enable_raw_mode().unwrap();
 
-    while running.load(Ordering::Relaxed) {
+      while running.load(Ordering::Relaxed) {
         for frame in &FRAMES {
-            if !running.load(Ordering::Relaxed) {
-                break;
-            }
+          if !running.load(Ordering::Relaxed) {
+              break;
+          }
 
-            // Clear previous frame
-            execute!(
-                stdout,
-                MoveTo(col, row),
-                Clear(ClearType::FromCursorDown)
+          queue!(stdout, SavePosition).unwrap();
+          
+          // render animation frame
+          for (i, line) in frame.lines().enumerate() {
+            queue!(
+              stdout,
+              MoveTo(col, row + i as u16),
+              Print(line),
             ).unwrap();
+          }
 
-            // Draw new frame
-            for (i, line) in frame.lines().enumerate() {
-                execute!(
-                    stdout,
-                    MoveTo(col, row + i as u16),
-                    Print(line)
-                ).unwrap();
-            }
+          // Restore cursor position and flush
+          queue!(stdout, RestorePosition).unwrap();
+          stdout.flush().unwrap();
 
-            thread::sleep(Duration::from_millis(200));
+          thread::sleep(Duration::from_millis(200));
         }
-    }
+      }
 
-    execute!(stdout, Show).unwrap();
+      disable_raw_mode().unwrap();
+    }));
+  }
+
+  fn stop(&mut self) {
+    self.running.store(false, Ordering::Relaxed);
+    if let Some(handle) = self.handle.take() {
+      handle.join().unwrap();
+    }
+  }
+}
+
+impl Drop for AnimationThread {
+  fn drop(&mut self) {
+    self.stop();
+  }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 
-    println!("   _   _     n _____ n   ____        ____    _   _       _         _         n  ___ n   ____       _____      ____      ");
-    println!("  | \\ |\"|    \\| ___\"|/  / __\"| n  n /\"___|  |'| |'|  n  /\"\\  n    |\"|         \\/\"_ \\/  / __\"| n   |_ \" _|  n |  _\"\\ n   ");
-    println!(" <|  \\| |>    |  _|\"   <\\___ \\/   \\| | n   /| |_| |\\  \\/ _ \\/   n | | n       | | | | <\\___ \\/      | |     \\| |_) |/   ");
-    println!(" n| |\\  |n    | |___    n___) |    | |/__  n|  _  |n  / ___ \\    \\| |/__  .-,_| |_| |  n___) |     /| |\\     |  _ <     ");
-    println!("  |_| \\_|     |_____|   |____/>>    \\____|  |_| |_|  /_/   \\_\\    |_____|  \\_)-\\___/   |____/>>   n |_|n     |_| \\_\\    ");
-    println!("  ||   \\\\,-.  <<   >>    )(  (__)  _// \\\\   //   \\\\   \\\\    >>    //  \\\\        \\\\      )(  (__)  _// \\\\_    //   \\\\_   ");
-    println!("  (_\")  (_/  (__) (__)  (__)      (__)(__) (_\") (\"_) (__)  (__)  (_\")(\"_)      (__)    (__)      (__) (__)  (__)  (__) \n\n");
+    println!("               _   _     n _____ n   ____        ____    _   _       _         _         n  ___ n   ____       _____      ____      ");
+    println!("              | \\ |\"|    \\| ___\"|/  / __\"| n  n /\"___|  |'| |'|  n  /\"\\  n    |\"|         \\/\"_ \\/  / __\"| n   |_ \" _|  n |  _\"\\ n   ");
+    println!("             <|  \\| |>    |  _|\"   <\\___ \\/   \\| | n   /| |_| |\\  \\/ _ \\/   n | | n       | | | | <\\___ \\/      | |     \\| |_) |/   ");
+    println!("             n| |\\  |n    | |___    n___) |    | |/__  n|  _  |n  / ___ \\    \\| |/__  .-,_| |_| |  n___) |     /| |\\     |  _ <     ");
+    println!("              |_| \\_|     |_____|   |____/>>    \\____|  |_| |_|  /_/   \\_\\    |_____|  \\_)-\\___/   |____/>>   n |_|n     |_| \\_\\    ");
+    println!("              ||   \\\\,-.  <<   >>    )(  (__)  _// \\\\   //   \\\\   \\\\    >>    //  \\\\        \\\\      )(  (__)  _// \\\\_    //   \\\\_   ");
+    println!("              (_\")  (_/  (__) (__)  (__)      (__)(__) (_\") (\"_) (__)  (__)  (_\")(\"_)      (__)    (__)      (__) (__)  (__)  (__) \n\n");
 
   // Parse CLI arguments first
   let args = Cli::parse();
   let password = args.npassword;
   let full_regex_pattern = format!(r"^npub1({})", args.nregex);
   
-  println!(" :: STARTING");
+  println!("             :: STARTING\r");
   let dance_logs = [
-      "it's reggae music time",
-      "DJ, set the beat now!",
-      "Time to shake it off!",
-      "Let's dance all night",
-      "Dance party, let's go!",
-      "Hands up maestro!",
-      "É hora do arrasta-pé",
-      "In the flow of the rhythm"
+    "it's reggae music time",
+    "DJ, set the beat now!",
+    "Time to shake it off!",
+    "Let's dance all night",
+    "Dance party, let's go!",
+    "Hands up maestro!",
+    "In the flow of the rhythm"
   ];
 
   let lets_dance = dance_logs.choose(&mut thread_rng()).unwrap();
-  println!(" :: {}", lets_dance);
-  println!(" :: Entropy algorithm in neschalostr are high affected in a positive way by people dancing");
+  println!("             :: {}\r", lets_dance);
+  println!("             :: Entropy algorithm in neschalostr are high affected in a positive way by people dancing\r");
 
   // Compile regex
   let re = Regex::new(&full_regex_pattern)
-      .map_err(|e| {
-          eprintln!("Error compiling regex: {}", e);
-          e
-      })?;
+    .map_err(|e| {
+        eprintln!("             :: Error compiling regex: {}\r", e);
+        e
+    })?;
 
-  println!(" :: REGEX /{}/", re.to_string());
+  println!("             :: REGEX /{}/\r", re.to_string());
 
-  let running = Arc::new(AtomicBool::new(true));
-  let animation_running = running.clone();
-
-  let animation_handle = thread::spawn(move || {
-      animate_character(animation_running, 16, 0);
-  });
-
-  // Give the animation a moment to start
+  let mut animation = AnimationThread::new();
+  animation.start(16, 0);
   thread::sleep(Duration::from_millis(500));
 
   // really start here
   loop {
-      // keys
-      let secret_key = Keys::generate();
-      
-      // npub
-      let bech32_pubkey = secret_key.public_key().to_bech32()?;
+    // keys
+    let secret_key = Keys::generate();
+    
+    // npub
+    let bech32_pubkey = secret_key.public_key().to_bech32()?;
 
-      // check if matches
-      if re.is_match(&bech32_pubkey) {
-          println!("--------------------");
-          println!("[{}]", Local::now().to_rfc3339());
-          println!("npub: {}", bech32_pubkey);
+    // check if matches
+    if re.is_match(&bech32_pubkey) {
+      println!("            --------------------");
+      println!("            [{}] npub: {}\r", Local::now().to_rfc3339(), bech32_pubkey);
 
-          // ncryptsec
-          let ncryptsec = EncryptedSecretKey::new(&secret_key.secret_key(), password.clone(), 16, KeySecurity::Medium).unwrap();
-          println!("ncryptsec: {}", ncryptsec.to_bech32()?);
-      }
+      // ncryptsec
+      let ncryptsec = EncryptedSecretKey::new(&secret_key.secret_key(), password.clone(), 16, KeySecurity::Medium).unwrap();
+      println!("            [{}] ncryptsec: {}\r", Local::now().to_rfc3339(), ncryptsec.to_bech32()?);
+    }
   }
 }
